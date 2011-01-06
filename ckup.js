e057528944f326996fa7b3e7afa6ed875c6c60d7
satyr <murky.satyr@gmail.com>
2011-01-06 19:33:14 +0900

    improved @element; added @A

--- ckup.js
+++ ckup.js
@@ -2,11 +2,11 @@
   var Ckup, tag, _ref, _i, _len, __clone = function(it){
     function fn(){ if (this.__proto__ !== it) this.__proto__ = it }
     return fn.prototype = it, new fn;
-  }, _fn = function(tag){
+  }, __slice = [].slice, _fn = function(tag){
     var tailless;
     tailless = tag === 'area' || tag === 'base' || tag === 'basefont' || tag === 'br' || tag === 'hr' || tag === 'img' || tag === 'input' || tag === 'link' || tag === 'meta';
-    return Ckup[tag] = function(head, body){
-      this.element(tag, head, body, tailless);
+    return Ckup[tag] = function(){
+      this.element(tag, arguments, tailless);
     };
   };
   Ckup = (_ref = typeof exports != 'undefined' && exports !== null ? exports : this.Ckup = {}, _ref.VERSION = '0.1.0', _ref.render = function(template){
@@ -37,29 +37,46 @@
     this._ += "<!DOCTYPE " + it + ">";
   }, _ref.text = function(it){
     this._ += this.quote(it);
-  }, _ref.element = function(name, atrs, body, tailless){
-    var code, key, val;
+  }, _ref.element = function(name, args, tailless){
+    var code, bodies, arg, key, val, body, _i, _len;
     code = '<' + name;
-    if (typeof atrs === 'object') {
-      for (key in atrs) {
-        val = atrs[key];
-        code += " " + key + "=\"" + this.quote(val) + "\"";
+    bodies = [];
+    for (_i = 0, _len = args.length; _i < _len; ++_i) {
+      arg = args[_i];
+      if (typeof arg === 'object') {
+        for (key in arg) {
+          val = arg[key];
+          code += " " + key + "=\"" + this.quote(val) + "\"";
+        }
+      } else {
+        bodies.push(arg);
       }
-    } else {
-      body = atrs;
     }
     if (tailless) {
       this._ += code + '/>';
       return;
     }
     this._ += code + '>';
-    if (typeof body === 'function') {
-      body = body.call(this);
-    }
-    if (body != null) {
-      this._ += body;
+    for (_i = 0, _len = bodies.length; _i < _len; ++_i) {
+      body = bodies[_i];
+      if (typeof body === 'function') {
+        body = body.call(this);
+      }
+      if (body != null) {
+        this._ += body;
+      }
     }
     this._ += "</" + name + "\n>";
+  }, _ref.A = function(url, txt){
+    var args;
+    args = __slice.call(arguments);
+    args[0] = {
+      href: url
+    };
+    if (!txt) {
+      args[1] = url;
+    }
+    this.element('a', args);
   }, _ref);
   for (_i = 0, _len = (_ref = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del', 'details', 'dfn', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'keygen', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'menu', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'tt', 'u', 'ul', 'video', 'xmp']).length; _i < _len; ++_i) {
     tag = _ref[_i];
