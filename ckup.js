cf08be51f0c47c610e8fb9f6d42c048f25f81bef
satyr <murky.satyr@gmail.com>
2011-01-28 14:32:40 +0900

    0.1.2

--- ckup.js
+++ ckup.js
@@ -1,22 +1,25 @@
 (function(){
-  var Ckup, tag, _ref, _i, _len, __clone = function(it){
+  var ckup, tag, _i, _ref, _len, __clone = function(it){
     function fn(){ if (this.__proto__ !== it) this.__proto__ = it }
     return fn.prototype = it, new fn;
   }, __slice = [].slice, _fn = function(tag){
     var tailless;
     tailless = tag === 'area' || tag === 'base' || tag === 'basefont' || tag === 'br' || tag === 'hr' || tag === 'img' || tag === 'input' || tag === 'link' || tag === 'meta';
-    return Ckup[tag] = function(){
+    return ckup[tag] = function(){
       this.element(tag, arguments, tailless);
     };
   };
-  Ckup = (_ref = typeof exports != 'undefined' && exports !== null ? exports : this.Ckup = {}, _ref.VERSION = '0.1.0', _ref.render = function(template){
+  ckup = typeof exports != 'undefined' && exports !== null ? exports : this.Ckup = {};
+  ckup.VERSION = '0.1.2';
+  ckup.render = function(template){
     var me, _ref;
     if (typeof template !== 'function') {
-      template = Function('with(this)' + (typeof Coco != 'undefined' && Coco !== null ? Coco : require('coco')).compile('' + template));
+      template = Function('with(this)' + (typeof Coco != 'undefined' && Coco !== null ? Coco : require('coco')).compile("" + template));
     }
     template.call(me = (_ref = __clone(this), _ref._ = '', _ref));
     return me._;
-  }, _ref.quote = (function(re, fn){
+  };
+  ckup.quote = (function(re, fn){
     return function(it){
       return ("" + it).replace(re, fn);
     };
@@ -33,11 +36,14 @@
     case '\'':
       return '&#39;';
     }
-  })), _ref.doctype = function(it){
+  }));
+  ckup.doctype = function(it){
     this._ += "<!DOCTYPE " + it + ">";
-  }, _ref.text = function(it){
+  };
+  ckup.text = function(it){
     this._ += this.quote(it);
-  }, _ref.element = function(name, args, tailless){
+  };
+  ckup.element = function(name, args, tailless){
     var code, bodies, arg, key, val, body, _i, _len;
     code = '<' + name;
     bodies = [];
@@ -67,7 +73,8 @@
       }
     }
     this._ += "</" + name + "\n>";
-  }, _ref.A = function(url, txt){
+  };
+  ckup.A = function(url, txt){
     var args;
     args = __slice.call(arguments);
     args[0] = {
@@ -77,10 +84,10 @@
       args[1] = url;
     }
     this.element('a', args);
-  }, _ref);
+  };
   for (_i = 0, _len = (_ref = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del', 'details', 'dfn', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'keygen', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'mark', 'menu', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'tt', 'u', 'ul', 'video', 'xmp']).length; _i < _len; ++_i) {
     tag = _ref[_i];
     _fn(tag);
   }
-  Ckup.$ = Ckup.quote;
+  ckup.$ = ckup.quote;
 }).call(this);
