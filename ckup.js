bad5ad3f8b71dcfa833fbc1858ae63fbd1a47bc8
satyr <murky.satyr@gmail.com>
2011-05-02 23:03:22 +0900

    0.1.5

--- ckup.js
+++ ckup.js
@@ -10,7 +10,7 @@
   ckup = typeof exports != 'undefined' && exports !== null
     ? exports
     : this.Ckup = {};
-  ckup.VERSION = '0.1.4';
+  ckup.VERSION = '0.1.5';
   ckup.render = function(template){
     var me, _ref;
     if (typeof template !== 'function') {
