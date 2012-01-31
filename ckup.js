278443add06e976dbacf13ec78af28511d60d0be
satyr <murky.satyr@gmail.com>
2012-01-31 11:56:42 +0900

    tiny doc fixes

--- ckup.js
+++ ckup.js
@@ -3,7 +3,7 @@
   ckup = typeof exports != 'undefined' && exports !== null
     ? exports
     : this.Ckup = {};
-  ckup.VERSION = '0.1.7';
+  ckup.VERSION = '0.1.8b';
   ckup.render = function(template, mixin){
     return this.renderer(template)(mixin);
   };
